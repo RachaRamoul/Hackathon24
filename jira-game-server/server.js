9f38f698720ec1369924f8e01e42fe5530007156
SALAMANI Sarah Lina <js_salamani@esi.dz>
2024-04-26 14:26:32 +0200

    nom et id des tickets

--- jira-game-server/server.js
+++ jira-game-server/server.js
@@ -15,34 +15,54 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'displayWinner.html'));
 });
 
+
+
+async function getResolvedIssueCount(user, time) {
+    // Calculer la date de début en fonction de la durée du minuteur
+    const startDate = new Date(Date.now() - (time * 60 * 1000)); // Soustraire la durée en minutes de la date actuelle
+
+    const response = await axios.get('https://hackathon-2024.atlassian.net/rest/api/3/search', {
+        params: {
+            jql: `project = "KAN" AND assignee = "${user}" AND status = Done AND resolutiondate >= "${startDate.toISOString()}"`,
+            fields: 'summary,key,id', // Assurez-vous que les IDs sont récupérés
+        },
+        headers: {
+            Authorization: `Basic ${encodedCredentials}`,
+            Accept: 'application/json',
+        },
+    });
+
+    const issues = response.data.issues.map(issue => ({
+        summary: issue.fields.summary,
+        key: issue.key,
+        id: issue.id, // Assurez-vous que l'ID est correctement récupéré
+    }));
+
+    return { count: response.data.issues.length, time: time, issues: issues };
+}
+
+
 app.get('/winner', async (req, res) => {
     const user1 = req.query.user1;
     const user2 = req.query.user2;
-    const time = parseInt(req.query.time) || 1; // Default time is set to 1 minute
+    const time = parseInt(req.query.time) || 1; 
     const user1Data = await getResolvedIssueCount(user1, time);
     const user2Data = await getResolvedIssueCount(user2, time);
+
+    // Convertir chaque objet issue en une chaîne de caractères
+    const user1Issues = user1Data.issues.map(issue => `${issue.summary} - ${issue.key} - ${issue.id}`);
+    const user2Issues = user2Data.issues.map(issue => `${issue.summary} - ${issue.key} - ${issue.id}`);
   
     res.json({ 
-      user1: { name: user1, count: user1Data.count, time: user1Data.time }, 
-      user2: { name: user2, count: user2Data.count, time: user2Data.time }
+      user1: { name: user1, count: user1Data.count, time: user1Data.time, issues: user1Issues }, 
+      user2: { name: user2, count: user2Data.count, time: user2Data.time, issues: user2Issues }
     });
 });
 
-async function getResolvedIssueCount(user, time) {
-    const response = await axios.get('https://hackathon-2024.atlassian.net/rest/api/3/search', {
-      params: {
-        jql: `project = "KAN" AND assignee = "${user}" AND status = Done`,
-        fields: 'summary',
-      },
-      headers: {
-        Authorization: `Basic ${encodedCredentials}`,
-        Accept: 'application/json',
-      },
-    });
-  
-    return { count: response.data.issues.length, time: time };
-  }
+
+
+
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
